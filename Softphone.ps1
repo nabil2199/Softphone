8a42730a4108c12d9eea7c9012b89e7f6bac1694
nabil2199 <nabil2199@gmail.com>
2016-10-26 15:03:40 +0200

    initial commit

---
+++ Softphone.ps1
@@ -0,0 +1,69 @@
+﻿<#
+Softphone User voice policy change
+Version 0.1
+OCWS
+#>
+<#
+param (
+	[Parameter(Position=0,
+				HelpMessage="Path to input CSV file")]
+	[alias("CSVPath")]
+	[String]$InputCSVPath="C:\Users\NXLX8474\Desktop\users.csv"
+)
+#>
+
+
+#User CSV loading
+$usersList = $null
+$usersList = Import-CSV C:\Sources\SoftphoneUsers.csv
+$count = $usersList.count
+
+write-host "User count within CSV file=" $count
+write-host ""
+
+ForEach ($user in $usersList) 
+{
+
+    if ($user.VoicePolicy -eq $null )
+        {
+        $CurrentUserObject = get-csuser -identity $user.upn
+        $CurrentVoicePolicy = ($CurrentUserObject).VoicePolicy
+
+        if($CurrentVoicePolicy.friendlyname -eq "VP-Full-Access")
+            {
+            Grant-csvoicepolicy -Identity $user.upn -PolicyName VP-BTIP-Full-Access 
+            }
+        elseif($CurrentVoicePolicy.friendlyname -eq "VP-Phone-Limited")
+            {
+            Grant-csvoicepolicy -Identity $user.upn -PolicyName VP-BTIP-Phone-Limited
+            }
+        elseif($CurrentVoicePolicy.friendlyname -eq "VP-BTIP-National-Limited")
+            {
+            Grant-csvoicepolicy -Identity $user.upn -PolicyName VP-BTIP-National-Limited
+            }
+        elseif($CurrentVoicePolicy.friendlyname -eq "VP-BTIP-National-CallCharge")
+            {
+            Grant-csvoicepolicy -Identity $user.upn -PolicyName VP-BTIP-National-CallCharge
+            }
+        elseif($CurrentVoicePolicy.friendlyname -eq "VP-BTIP-National")
+            {
+            Grant-csvoicepolicy -Identity $user.upn -PolicyName VP-BTIP-National 
+            }
+        elseif($CurrentVoicePolicy.friendlyname -eq "VP-BTIP-International")
+            {
+            Grant-csvoicepolicy -Identity $user.upn -PolicyName VP-BTIP-International
+            }
+        }
+	
+    elseif ($user.VoicePolicy -ne NULL)
+        {
+        Grant-CsVoicePolicy -Identity $user.upn -PolicyName $user.VoicePolicy
+	    if ($? -eq $true)
+            {
+		    write-host -NoNewLine "Voice policy "; Write-Host -NoNewline -ForegroundColor Green $user.VoicePolicy; Write-Host -NoNewline " granted to useruser: "; Write-Host -ForegroundColor Cyan $user.upn
+	        }
+	    }
+
+	write-host "****************************User " -nonewline; Write-Host -foregroundcolor Cyan $user.upn -nonewline; Write-Host  " set****************************"
+	write-host ""
+}	
